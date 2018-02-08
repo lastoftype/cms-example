46248dd5e66ee583a333081235182dbebe4fa10e
Matt Unhjem <lastoftype@users.noreply.github.com>
2018-02-07 21:40:25 -0500

    Ejected and added sass-loader

--- src/api/config.js
+++ src/api/config.js
@@ -1,8 +1,2 @@
-// Prod ?
-export const APP_BASE_URL = 'http://localhost:3000/ng/';
-export const API_BASE_URL = 'http://localhost:3000/api/ng/';
-export const BASE_URL = 'http://localhost:8001/';
-export const LMS_BASE_URL = 'http://localhost:8000/';
-
 // Local 
 export const LOCAL_API_BASE = 'http://localhost:5000/';
--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Input from './Input';
 
 class App extends Component {
   render() {
     return (
       <div className="app-home">
-        
+          <Input />
       </div>
     );
   }
