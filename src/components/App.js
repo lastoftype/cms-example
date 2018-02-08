e9558fdb0fae239284fff300e5503871f90ed514
Matt Unhjem <lastoftype@users.noreply.github.com>
2018-02-07 23:31:38 -0500

    Added button and input styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import HomeHeader from './HomeHeader';
+import Input from './Input';
 
 class App extends Component {
   render() {
     return (
       <div className="app-home">
           <HomeHeader />
+          <section className="editor">
+          	<div className="tile">
+          		<div className="form-group">
+	          		<label>Content</label>
+	          		<textarea className="form-control" />
+	          	</div>
+	          	<button className="button button-primary">Submit</button>
+          	</div>
+          </section>
       </div>
     );
   }
